14bf2b78a310e728ccd0a3f000502abeb2fe6cf5
Alex Mirran <90220293+galaxyxtwo@users.noreply.github.com>
2023-10-11 21:50:30 -0500

    Added input box and local storage

--- cid-leaderboard/src/App.js
+++ cid-leaderboard/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState({ items: [], error: null });
-
+  const [cidInput, setCidInput] = useState('');  // 1. State to handle the input CID
+  const initialCidsFromLocalStorage = JSON.parse(localStorage.getItem('savedCids')) || [];
+  const [cids, setCids] = useState(initialCidsFromLocalStorage);
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -13,12 +16,7 @@ function App() {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            cids: [
-              'QmYipSwcyLsv9FYndT1aUrA5FpBFAA2zYGstq1enjPLTyj',
-              'bafkreibm6ku4kcsbzlby3c6st57cyk2t5vztgvntwxkp56xtqdxqq5oqhi',
-              'bafkreicml3xokhxca4h3qvxas42u54ynqtjpkpbyodjjh2sr2xg7g6nkgq'
-              
-            ],
+            cids: cids,
             page: 1,
             size: 5
           })
@@ -29,6 +27,7 @@ function App() {
         }
 
         const fetchedData = await response.json();
+        console.log("API Response:", fetchedData);  // Log the API response
         if (fetchedData.success && fetchedData.data && Array.isArray(fetchedData.data.data)) {
             setData({ items: fetchedData.data.data, error: null });
         } else {
@@ -41,16 +40,37 @@ function App() {
     };
 
     fetchData();
-  }, []); // Empty dependency array means useEffect will only run once, similar to componentDidMount
+  }, [cids]);  // 3. Listen to the change of the cids state
+
+  const handleAddCid = () => {
+    if (cidInput) {
+      setCids(prevCids => {
+        const newCids = [...prevCids, cidInput.trim()];  
+        localStorage.setItem('savedCids', JSON.stringify(newCids));  // Save to local storage
+        return newCids;
+      });
+      setCidInput('');
+    }
+};
 
   return (
     <div className="App">
-    <nav className="App-navbar">
-        {/* You can add navbar content here, e.g., links, logo, etc. */}
-        <span>The most popular NFTs on my social app</span>
-    </nav>
+      <nav className="App-navbar">
+          <span>My NFT collection</span>
+      </nav>
       <h1>Leaderboard</h1>
-      <h2>Private activity data for NFTs on IPFS!</h2>
+      <h2>The most popular NFTs</h2>
+
+      <div className="cid-input-section">  {/* 2. Add a text input and a button */}
+        <input 
+          type="text" 
+          value={cidInput} 
+          onChange={e => setCidInput(e.target.value)} 
+          placeholder="Enter CID to query..."
+        />
+        <button onClick={handleAddCid}>Add to Leaderboard</button>
+      </div>
+
       <table className="leaderboard-table">
         <thead>
           <tr>
@@ -58,22 +78,21 @@ function App() {
             <th>Total Views</th>
           </tr>
         </thead>
-<tbody>
-  {
-    [...data.items].sort((a, b) => b.numbersAccessed - a.numbersAccessed)
-    .map((item, index) => (
-      <tr key={index}>
-        <td className="cid-column">
-          <a href={`https://leto.gg/ipfs/${item.cid}`} target="_blank" rel="noopener noreferrer">
-            {item.cid}
-          </a>
-        </td>
-        <td>{item.numbersAccessed}</td>
-      </tr>
-    ))
-  }
-</tbody>
-
+        <tbody>
+          {
+            [...data.items].sort((a, b) => b.numbersAccessed - a.numbersAccessed)
+            .map((item, index) => (
+              <tr key={index}>
+                <td className="cid-column">
+                  <a href={`https://leto.gg/ipfs/${item.cid}`} target="_blank" rel="noopener noreferrer">
+                    {item.cid}
+                  </a>
+                </td>
+                <td>{item.numbersAccessed}</td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
       {data.error && <p>Error: {data.error.message}</p>}
     </div>
@@ -81,4 +100,3 @@ function App() {
 }
 
 export default App;
-
